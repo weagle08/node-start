d25b31f809e5b2e634d05aa8a56be0e36f4de8e2
Benjamin Johnson <benjamin.johnson@bluehalo.com>
2022-10-07 19:11:41 -0500

    updated for node class

--- src/constants/QueryDefaults.ts
+++
@@ -1,2 +0,0 @@
-export const LIMIT_MAX = 500;
-export const LIMIT_DEFAULT = 10;
--- src/enums/DatabaseActions.ts
+++
@@ -1,5 +0,0 @@
-export enum DatabaseActions {
-    NO_ACTION = 'NO ACTION',
-    CASCADE = 'CASCADE',
-    SET_NULL = 'SET NULL'
-}
